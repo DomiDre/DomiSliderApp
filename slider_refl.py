Bug fixes and corrected chi2 calculation in main app.

slider_refl.py
@@ -4,8 +4,8 @@ import lmfit
 import numpy as np
 class cPlotAndFitRefl(cPlotAndFit):
     def __init__(self, parent=None):
-        self.modelfile = "refl_modelfile.dat"
-        self.sldmodelfile = "sld_modelfile.dat"
+        self.reflsavefile = "gui_refl.dat"
+        self.sldsavefile = "gui_sld.dat"
         self.data_path = None
         super().__init__(parent)
         
@@ -15,7 +15,7 @@ class cPlotAndFitRefl(cPlotAndFit):
         
         if self.x is not None and self.y is not None and self.sy is not None:
             self.ax1.errorbar(self.x, self.y, self.sy, marker='.',\
-                    linestyle='None', color='#4dac26', label=self.data_path)
+                    linestyle='None', color='#2c7bb6', label=self.data_path)
 
         self.model_plot, = self.ax1.plot(self.x, self.ymodel, marker='None',\
                 linestyle='-', color='#ca0020', lw=1, label="Model")
@@ -29,19 +29,21 @@ class cPlotAndFitRefl(cPlotAndFit):
         self.ax1.set_xlabel("$\mathit{q_z} \, / \, \AA^{-1}$")
         self.ax1.set_ylabel("$\mathit{I} \, / \, a.u.$")
         
-        
-        self.sld_plot, = self.ax2.plot(self.xsld/10., self.ysld*1e6,\
+        self.sld_plot, = self.ax2.plot(self.xsld/10., np.real(self.ysld)*1e6,\
                 marker='None', ls='-', color='#e66101')
+        self.sld_plot_imag, = self.ax2.plot(self.xsld/10., np.imag(self.ysld)*1e6,\
+                marker='None', ls='-', color='red')
         self.ax2.set_xlim(min(self.xsld/10.), max(self.xsld/10.))
+        self.ax2.set_ylim(0, max(np.real(self.ysld)*1e6)*1.2)
         self.ax2.set_xlabel("$\mathit{z} \, / \, nm$")
         self.ax2.set_ylabel("$\mathit{SLD} \, / \, 10^{-6} \AA^{-2}$")
         self.fig.subplots_adjust(wspace=0.5)
         
     def update_plot(self):
         self.get_model(self.p)
-
         self.model_plot.set_ydata(self.ymodel)
-        self.sld_plot.set_ydata(self.ysld*1e6)
+        self.sld_plot.set_ydata(np.real(self.ysld)*1e6)
+        self.sld_plot_imag.set_ydata(np.imag(self.ysld)*1e6)
         self.draw()
         
     def figure_of_merit(self, p):
@@ -49,7 +51,7 @@ class cPlotAndFitRefl(cPlotAndFit):
         return (np.log(self.ymodel)-np.log(self.y))/self.sy*self.y
         
     def export_model(self):
-        savefile = open(self.modelfile, "w")
+        savefile = open(self.reflsavefile, "w")
         if self.fit_result is not None:
             savefile.write("#"+lmfit.fit_report(self.fit_result).replace("\n", "\n#"))
 
@@ -63,15 +65,20 @@ class cPlotAndFitRefl(cPlotAndFit):
             savefile.write("#q / A-1 \t Imodel / a.u.\n")
             for iq, qval in enumerate(self.x):
                 savefile.write(str(qval) + "\t" + str(self.ymodel[iq])+"\n")
-        print("Wrote results to " + self.modelfile)
+        print("Wrote results to " + self.reflsavefile)
         savefile.close()
         
-        savefile = open(self.sldmodelfile, "w")
+        savefile = open(self.sldsavefile, "w")
         if self.fit_result is not None:
             savefile.write("#"+lmfit.fit_report(self.fit_result).replace("\n", "\n#"))
 
-        savefile.write("#z / nm \t SLD / 1e-6 A-2\n")
+        savefile.write("\n#z / nm \t real(SLD) / 1e-6 A-2\t imag(SLD) / 1e-6 A-2\n")
         for iq, xval in enumerate(self.xsld):
-            savefile.write(str(xval/10.) +"\t"+ str(self.ysld[iq]*1e6)+"\n")
-        print("Wrote results to " + self.sldmodelfile)
+            savefile.write(str(xval/10.) +"\t"+ str(np.real(self.ysld[iq])*1e6)+\
+            "\t"+ str(np.imag(self.ysld[iq])*1e6)+"\n")
+        print("Wrote results to " + self.sldsavefile)
         savefile.close()
+        
+    def save_plot(self):
+        self.fig.savefig(self.reflsavefile.rsplit(".",1)[0]+"_plot.png")
+